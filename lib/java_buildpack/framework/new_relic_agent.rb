4bf03578f3add8b3b12b83dce6e43971063f18fa
Paul Grave <paul.grave@bskyb.com>
2015-02-18 14:14:53 +0000

    fixed: bad tests

--- lib/java_buildpack/framework/new_relic_agent.rb
+++ lib/java_buildpack/framework/new_relic_agent.rb
@@ -40,9 +40,9 @@ module JavaBuildpack
         .add_system_property('newrelic.config.app_name', "'#{application_name}'")
         .add_system_property('newrelic.config.log_file_path', logs_dir)
         @droplet.java_opts.add_system_property('newrelic.enable.java.8', 'true') if @droplet.java_home.version[1] == '8'
-        @droplet.java_opts.add_system_property('newrelic.config.proxy_host', proxy_host) if proxy_host?
-        @droplet.java_opts.add_system_property('newrelic.config.proxy_password', proxy_password) if proxy_password?
-        @droplet.java_opts.add_system_property('newrelic.config.proxy_port', proxy_port) if proxt_port?
+        @droplet.java_opts.add_system_property('newrelic.config.proxy_host', proxy_host) if !proxy_host.nil? and !proxy_host.empty?
+        @droplet.java_opts.add_system_property('newrelic.config.proxy_password', proxy_password) if !proxy_password.nil? and !proxy_password.empty?
+        @droplet.java_opts.add_system_property('newrelic.config.proxy_port', proxy_port) if !proxy_port.nil? and !proxy_port.empty?
       end
 
       protected
